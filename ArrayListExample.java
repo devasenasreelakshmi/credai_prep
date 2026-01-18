4bab1d08c831d00520d41399561d6e49a64f52b7
devasenasreelakshmi <devasenasreelakshmi@gmail.com>
2026-01-18 13:49:39 +0530

    Add files via upload

---
+++ ArrayListExample.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class ArrayListExample{
+    public static void main(String[] args) {
+        ArrayList<String> cars=new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+
+        System.out.println(cars);
+    }
+}
